04cd37daa44edbcfa3f89fdfec005972d6ccf641
Marina <lav_mari@mail.ru>
2024-10-27 08:19:14 +0300

    with tests

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+import json
+import os
+
+
+def read_json(path: str) -> dict:
+    full_path = os.path.abspath(path)
+    with open(full_path, "r", encoding="UTF-8") as file:
+        data = json.load(file)
+
+    return data
