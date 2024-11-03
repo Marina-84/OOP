ff77e268a0e3f11743df03c8f5a9d2b41bb643e3
Marina <lav_mari@mail.ru>
2024-11-04 01:46:03 +0300

    Исправлено

--- src/products.py
+++ src/products.py
@@ -1,7 +1,7 @@
 class Product:
     name = str
     description = str
-    price = int
+    price = float
     quantity = int
 
     def __init__(self, name, description, price, quantity):
@@ -9,3 +9,13 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+
+if __name__ == "__main__":
+    product1 = Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+    print(product2.name)
+    print(product2.description)
+    print(product2.price)
+    print(product2.quantity)
