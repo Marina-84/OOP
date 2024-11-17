84ab555dc671e50c7e6644cb665c11c5f20be708
Marina <lav_mari@mail.ru>
2024-11-17 05:57:14 +0300

    на проверку

--- tests/test_category.py
+++ tests/test_category.py
@@ -4,15 +4,21 @@ from src.category import Category
 def test_category_init(second_category):
     assert second_category.name == "Телевизоры"
     assert second_category.description == "Современный телевизор, который станет вашим другом"
-    assert second_category.products == [
+    assert second_category.products_list == [
         {"name": '55" QLED 4K', "description": "Фоновая подсветка", "price": 123000.0, "quantity": 7}
     ]
     assert Category.category_count == 1
-    assert len(second_category.products) == 1
+    assert len(second_category.products_list) == 1
     assert Category.product_count == 1
 
 
 def test_empty_list(category3):
-    assert category3.products == []
+    assert category3.products_list == []
     assert category3.name == "Ноутбуки"
     assert category3.description == "Современный ноутбук по доступной цене"
+
+
+def test_category_products_property(category_property):
+    assert category_property.products == (
+        "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n" "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n"
+    )
--- tests/test_products.py
+++ tests/test_products.py
@@ -1,5 +1,28 @@
+from src.products import Product
+
+
 def test_product_init(product):
     assert product.name == "Samsung"
     assert product.description == "256GB, Серый цвет, 200MP камера"
     assert product.price == 180000.0
     assert product.quantity == 5
+
+
+def test_new_product():
+    new_product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    new_product.name = "Samsung Galaxy S23 Ultra"
+    new_product.description = "256GB, Серый цвет, 200MP камера"
+    new_product.price = 180000.0
+    new_product.quantity = 5
+
+
+def test_price_setter(capsys):
+    new_product = Product.new_product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    new_product.price = 99999
+    assert new_product.price == 99999
+    new_product.price = 0
+    message = capsys.readouterr()
+    assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
+    assert new_product.price == 99999
+    new_product.price = 100
+    assert new_product.price == 100
